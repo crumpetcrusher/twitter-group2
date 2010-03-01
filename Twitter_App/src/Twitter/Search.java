97bb9607e1471cd5d08a1b5de0ab2c7e47b5b728
Soljia <Soljia@6d042aa6-1d97-11df-9e2b-99333c9bc362>
2010-03-01 16:27:45 +0000

    HttpClient shit removed

--- Twitter_App/src/Twitter/Search.java
+++ Twitter_App/src/Twitter/Search.java
@@ -2,9 +2,6 @@ package Twitter;
 import java.io.*;
 import java.util.ArrayList;
 
-import org.apache.commons.httpclient.methods.RequestEntity;
-import org.apache.commons.httpclient.methods.StringRequestEntity;
-
 import Exceptions.Exception_NegativeNumber;
 //import 
 
