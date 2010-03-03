9bb3a1d317d56765483622086efb96f7a8d0c03b
Soljia <Soljia@6d042aa6-1d97-11df-9e2b-99333c9bc362>
2010-03-03 01:24:54 +0000

    Removed all warnings for the class AlphabeticallyTweets by commenting and suppressing.

--- Twitter_App/src/SortingClasses/AlphabeticallyTweet.java
+++ Twitter_App/src/SortingClasses/AlphabeticallyTweet.java
@@ -4,19 +4,49 @@ import java.util.Comparator;
 
 import Twitter.Tweet;
 
+/**
+ * This class is for the sort method.
+ * @author Rick Humes
+ *
+ */
+@SuppressWarnings("unchecked")
 public class AlphabeticallyTweet implements Comparator{
 
+	
+	//Class Variables
+	/**
+	 * Whether or not to sort by descending.
+	 */
 	private boolean descending = true;
 	
+	//Constructors
+	
+	/**
+	 * Constructor allows for no inputs. Automatically does descending order.
+	 */
 	public AlphabeticallyTweet()
 	{
 	}
 	
+	/**
+	 * Constructor allows for the user to decide to order by ascending or descending.
+	 * 
+	 * @param isDescending Descending = true, Ascending = false;
+	 */
 	public AlphabeticallyTweet(boolean isDescending)
 	{
 		descending = isDescending;
 	}
 	
+	//Methods
+	
+	/**
+	 * This method is an override in the Comparator class 
+	 * 
+	 * @param tweet
+	 * @param anotherTweet
+	 * @return Return is only needed for the comparator
+	 */
 	public int compare(Object tweet, Object anotherTweet)
 	{
 		int sortInt;
