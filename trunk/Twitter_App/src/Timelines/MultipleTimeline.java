758255d77d1a540008bef56a55befd2e194c2c19
Soljia <Soljia@6d042aa6-1d97-11df-9e2b-99333c9bc362>
2010-03-05 17:00:25 +0000

    Multiple timeline implented

--- trunk/Twitter_App/src/Timelines/MultipleTimeline.java
+++ trunk/Twitter_App/src/Timelines/MultipleTimeline.java
@@ -1,5 +1,53 @@
 package Timelines;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import Twitter.Tweet;
+import Twitter.Tweeter;
+
 public class MultipleTimeline extends Timeline {
+	
+	private List<Timeline> timelines = new ArrayList<Timeline>();
+	
+	public MultipleTimeline(String[] userIDs)
+	{
+		for(String userID : userIDs)
+			addUserTimeline(new Tweeter(userID));
+	}
+	
+	public MultipleTimeline(Tweeter[] tweeters)
+	{
+		for(Tweeter tweeter : tweeters)
+			addUserTimeline(tweeter);
+	}
+	
+	public MultipleTimeline(Timeline[] newTimelines)
+	{
+		for(Timeline timeline : newTimelines)
+			addTimeline(timeline);
+	}
+	
+	public void addUserTimeline(Tweeter tweeter)
+	{
+		UserTimeline temp = new UserTimeline(tweeter);
+		addTimeline(temp);
+	}
+	
+	public void addTimeline(Timeline timeline)
+	{
+		timelines.add(timeline);
+		updateTweets();
+	}
+	
+	public void updateTweets()
+	{
+		List<Tweet> temp = new ArrayList<Tweet>();
+		for(Timeline timeline : timelines)
+			for(Tweet tweet : timeline.getTweets())
+				temp.add(tweet);
+		clear();
+		setTweets((Tweet[])temp.toArray());
+	}
 
 }
