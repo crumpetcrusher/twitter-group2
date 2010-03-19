f501a689f957f08c7999112119037d5f4622f573
Soljia <Soljia@6d042aa6-1d97-11df-9e2b-99333c9bc362>
2010-03-19 15:31:34 +0000

    Commented the SortingClasses

--- Twitter_App/src/SortingClasses/TweetComparer.java
+++ Twitter_App/src/SortingClasses/TweetComparer.java
@@ -4,6 +4,11 @@ import java.util.Comparator;
 
 import Twitter.Tweet;
 
+/**
+ * This is an interface that is empty, it is meant to be able to distinguish CompareTypes
+ * @author rah323
+ */
 public interface TweetComparer extends Comparator<Tweet>{
 
+	
 }
