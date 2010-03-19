5df76260c5ea03c8bbf3c6d631f6d26a5c728593
Soljia <Soljia@6d042aa6-1d97-11df-9e2b-99333c9bc362>
2010-03-19 15:34:39 +0000

    ChronologicalTweet didn't commit for some reason.

--- trunk/Twitter_App/src/SortingClasses/ChronologicalTweet.java
+++ trunk/Twitter_App/src/SortingClasses/ChronologicalTweet.java
@@ -1,23 +1,48 @@
-	package SortingClasses;
+package SortingClasses;
 
-import java.util.Comparator;
 import java.util.Date;
 import Twitter.Tweet;
 
-@SuppressWarnings("unchecked")
+/**
+ * This class allows for sorting chronologically using the .sort method.
+ * @author rah323
+ */
 public class ChronologicalTweet implements TweetComparer {
-
+	
+	//Class Variables
+	
+	/**
+	 * Variable that stores whether or not the tweets should be sorted descending or ascending.
+	 */
 	private boolean descending = true;
 	
+	//Constructors
+	
+	/**
+	 * Constructor allows for no inputs. Automatically does descending order.
+	 */
 	public ChronologicalTweet()
 	{
 	}
 	
+	/**
+	 * Constructor that accepts an boolean whether or not to do descending..
+	 * 
+	 * @param isDescending Descending = true, Ascending = false;
+	 */
 	public ChronologicalTweet(boolean isDescending)
 	{
 		descending = isDescending;
 	}
 	
+	//Methods
+	
+	/**
+	 * This method is used specifically by Comparator class extended in TweetComparer
+	 * @param tweet 
+	 * @param anotherTweet 
+	 * @return <font color=red>Used only by the Comparator class</font>
+	 */
 	public int compare(Tweet tweet, Tweet anotherTweet)
 	{
 		int sortInt;
