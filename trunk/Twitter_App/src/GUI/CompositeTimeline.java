12806d3404639d6385b1736bd91616db908431c9
scott.smiesko <scott.smiesko@6d042aa6-1d97-11df-9e2b-99333c9bc362>
2010-04-03 05:52:20 +0000

    Start of a GUI, creating GUI object containers and such first.  VERY WIP :D
    CompositeTimeline - Displays a list of TweetContainers (TweetContainers are passed in like a stack, latest gets popped up the stack first, followed by the following tweets
    
    TweetContainer - JPanel container with a BorderLayout for showing the Username,Icon,Text, and Source + Date.

---
+++ trunk/Twitter_App/src/GUI/CompositeTimeline.java
@@ -0,0 +1,18 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Displays all of the TweetContainer GUI objects in a "Timeline" feed
+ * @author Frappe051
+ *
+ */
+public class CompositeTimeline extends JScrollPane{
+	
+	JPanel Display = null;
+	
+	
+
+
+}
