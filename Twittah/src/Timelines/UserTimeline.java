a723ba573a435c07f5ce1d47906da05751146a09
Soljia <Soljia@6d042aa6-1d97-11df-9e2b-99333c9bc362>
2010-04-29 23:05:28 +0000

    UserTimeline 100% Complete Commented All Println's taken out

--- Twittah/src/Timelines/UserTimeline.java
+++ Twittah/src/Timelines/UserTimeline.java
@@ -1,159 +1,133 @@
 package Timelines;
 
+//Import Statements
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 import javax.swing.ImageIcon;
-
 import org.w3c.dom.DOMException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
-
-import Changes.DisplayItem;
 import Changes.Timeline;
-import Exceptions.TweeterException;
 import Twitter.Tweet;
 import Twitter.Tweeter;
 import backend.XMLHelper;
 
+
+//The UserTimeline class!
 public class UserTimeline extends Timeline {
-
-	private Tweeter tweeter = null;
-	Thread thread = null;
-
-	private UserTimeline()
-	{
-		
-	}
-	
-	public UserTimeline(Tweeter newTweeter) throws TweeterException {
-		tweeter = newTweeter;
-		System.out.println("Creating User Timeline for "
-				+ tweeter.getUserName());
-		downloadAndParse();
-	}
-
-
-	public String toString()
-	{
-		return tweeter.getUserName();
-	}
-	private void downloadXML() throws TweeterException {
-		System.out.println("Downloading User Timeline.");
-		timelineXML = XMLHelper.getTweetsByScreenName(tweeter.getUserName());
-	}
-	
-	public static UserTimeline parseFromDocument(Document doc)
-	{
-		UserTimeline temp = new UserTimeline();
-		temp.timelineXML = doc;
-		temp.parseXML();
-		return temp;
-	}
-
-	private void parseXML(){
-		System.out.println("Parsing UserTimeline XML");
-
-		Element statuses;
-		Element status;
-		Element id;
-		Element text;
-		Element method;
-		Element date;
-		Element tweeterName;
-		Element tweeterIcon;
-		Element tweeterEle;
-
-		NodeList allStatuses;
-
-		statuses = (timelineXML.getDocumentElement());
-
-		allStatuses = statuses.getElementsByTagName("status");
-
-		for (int t = 0; t < allStatuses.getLength(); ++t) {
-			status = (Element) (allStatuses.item(t));
-
-			if(tweeter == null)
-			{
-				tweeterEle = (Element) status.getElementsByTagName("user").item(0);
-				tweeterName = (Element) tweeterEle.getElementsByTagName("name").item(0);
-				tweeterIcon = (Element) tweeterEle.getElementsByTagName("profile_image_url").item(0);
-				try {
-					tweeter = new Tweeter(tweeterName.getTextContent(), new ImageIcon(new URL(tweeterIcon.getTextContent())), this);
-					while(tweeter == null) {}
-				} catch (DOMException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (MalformedURLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-			
-			id = (Element) status.getElementsByTagName("id").item(0);
-			String tweetID = id.getTextContent();
-
-			text = (Element) status.getElementsByTagName("text").item(0);
-			String tweetText = text.getTextContent();
-
-			String pattern = "</?.*?>";
-			method = (Element) status.getElementsByTagName("source").item(0);
-			String tweetMethod = method.getTextContent();
-			
-			Matcher matcher = Pattern.compile(pattern).matcher(tweetMethod);
-			while (matcher.find()) {
-				tweetMethod = matcher.replaceAll("");
-			}
-
-			date = (Element) status.getElementsByTagName("created_at").item(0);
-			String tweetDate = date.getTextContent();
-			
-			Tweet tweet = new Tweet(tweeter, tweetID, tweetText, new Date(tweetDate), tweetMethod);
-
-			addDisplayItem(tweet);
-		}
-	}
-
-	public void downloadAndParse() {
-        thread = (new Thread() {
-            public void run() {
-                            try {
-                    			downloadXML();
-                    			if(timelineXML != null)
-                    				parseXML();
-                    			timelineRefreshed();
-                    			suspend();
-                            } catch (TweeterException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
+    
+    //Stores the tweeter who this timeline belongs to.
+    private Tweeter tweeter = null;
+    
+    //Thread that will download and parse xml seperately from the main thread
+    Thread thread = (new Thread() {
+        public void run() {
+                downloadXML();
+                if(timelineXML != null)
+                    parseXML();
+                timelineRefreshed();
+                suspend();
+                }
+        });
+    
+    //This constructor allows for the creation of a UserTimeline via parseFromDocument
+    private UserTimeline(){}
+    
+    
+    //This constructor allows you to pass in a tweeter and get his/her timeline
+    public UserTimeline(Tweeter newTweeter)
+    {
+        tweeter = newTweeter;
+        downloadAndParse();
+    }
+    
+    //Returns a UserTimeline based off of a document
+    public static UserTimeline parseFromDocument(Document doc)
+    {
+        UserTimeline temp = new UserTimeline();
+        temp.timelineXML = doc;
+        temp.parseXML();
+        return temp;
+    }
+    
+    //Downloads the XML neccesary for parsing
+    private void downloadXML()
+    {
+        timelineXML = XMLHelper.getTweetsByScreenName(tweeter.text());
+    }
+    
+    //Parses the XML passed in
+    @SuppressWarnings("deprecation")
+    private void parseXML()
+    {
+        Element statuses, status, text, method, date, tweeterName, tweeterIcon, tweeterEle;
+        NodeList allStatuses;
+        
+        statuses = (timelineXML.getDocumentElement());
+        allStatuses = statuses.getElementsByTagName("status");
+        
+        for (int t = 0; t < allStatuses.getLength(); ++t)
+        {
+            status = (Element) (allStatuses.item(t));
+            
+            //Creates a tweeter if there is not one
+            if(tweeter == null)
+            {
+                tweeterEle = (Element) status.getElementsByTagName("user").item(0);
+                tweeterName = (Element) tweeterEle.getElementsByTagName("name").item(0);
+                tweeterIcon = (Element) tweeterEle.getElementsByTagName("profile_image_url").item(0);
+                try 
+                {
+                    tweeter = new Tweeter(tweeterName.getTextContent(), new ImageIcon(new URL(tweeterIcon.getTextContent())), this);
+                    while(tweeter == null) {}
+                }
+                catch (DOMException e) 
+                {
+                    e.printStackTrace();
+                }
+                catch (MalformedURLException e) 
+                {
+                    e.printStackTrace();
+                }
             }
-    });
-        	thread.start();
-	}
-	/*
-	@Override
-	public Element toElement(Document doc) {
-		Element temp = doc.createElement("Timeline");
-		Element tweets = doc.createElement("Tweets");
-		temp.setAttribute("Type", "User");
-		temp.setTextContent(tweeter.getUserName());
-		
-		for(DisplayItem item : displayItems)
-			tweets.appendChild(item.toElement(doc));
-		
-		temp.appendChild(tweets);
-		return temp;
-	}*/
 
-	@Override
-	public void saveTimeline() {
-		XMLHelper.writeDocument(timelineXML, "src/usertimeline_" + tweeter.getUserName() + ".xml");
-		
-	}
+            text = (Element) status.getElementsByTagName("text").item(0);
+            String tweetText = text.getTextContent();
+
+            method = (Element) status.getElementsByTagName("source").item(0);
+            String tweetMethod = method.getTextContent();
+            
+            //Replaces any HTML tags
+            Matcher matcher = Pattern.compile("</?.*?>").matcher(tweetMethod);
+            while (matcher.find()) {
+                    tweetMethod = matcher.replaceAll("");
+            }
+
+            date = (Element) status.getElementsByTagName("created_at").item(0);
+            String tweetDate = date.getTextContent();
+            
+            Tweet tweet = new Tweet(tweeter, tweetText, new Date(tweetDate), tweetMethod);
+
+            addDisplayItem(tweet);
+        }
+        
+    }
+
+    //Downloads and Parses the user's timeline
+    public void downloadAndParse() 
+    {
+        thread.start();
+    }
+
+    //Saves the user's timeline to a local XML file
+    @Override
+    public void saveTimeline() 
+    {
+        XMLHelper.writeDocument(timelineXML, "src/usertimeline_" + tweeter.text() + ".xml");
+    }
 	
 }
