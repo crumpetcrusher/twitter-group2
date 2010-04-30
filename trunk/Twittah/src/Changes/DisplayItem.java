fa748ab5549507a57c502a0989a78a6928926238
Soljia <Soljia@6d042aa6-1d97-11df-9e2b-99333c9bc362>
2010-04-30 13:40:08 +0000

    DisplayItem commented

--- trunk/Twittah/src/Changes/DisplayItem.java
+++ trunk/Twittah/src/Changes/DisplayItem.java
@@ -1,8 +1,34 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Project      : IST240 - Twitter Application
+//
+// Interface Name   : DisplayItem
+//    
+// Authors      : Scott Smiesko, Rick Humes
+// Date         : 2010-30-04
+//
+//
+// DESCRIPTION
+// This class is a template for any item that wishes to be displayed in a timeline. Must return all of the following
+// method with proper information.
+//
+// KNOWN LIMITATIONS
+// None.
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 package Changes;
 
 public interface DisplayItem {
-
-    //public Twitter.Tweeter tweeter();
+    
+    // This class has 5 components used to relay information about a item to be displayer in a timeline:
+    //
+    // owner()          : The owner of this display item, usually a tweeter.
+    //
+    // source()         : The medium used to make this display item (most of the time web, twitteroid, Seesmic)
+    //
+    // text()           : The body, if you will, of the item.
+    //
+    // icon()           : The icon associated with this item, usually a tweeters user picture.
     
     public String owner();
 		
