d8bc39ee56bae3effb00cf87b6883aa1531f2132
Soljia <Soljia@6d042aa6-1d97-11df-9e2b-99333c9bc362>
2010-04-30 13:51:38 +0000

    Commented

--- trunk/Twittah/src/Changes/OrganizeType.java
+++ trunk/Twittah/src/Changes/OrganizeType.java
@@ -1,3 +1,31 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Project      : IST240 - Twitter Application
+//
+// Enum Name    : OrganizeType
+//    
+// Authors      : Scott Smiesko, Rick Humes
+// Date         : 2010-30-04
+//
+//
+// DESCRIPTION
+// This is just a enum that is used for the DisplayItemOrganizer.
+//
+// KNOWN LIMITATIONS
+// None.
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 package Changes;
+
+//Description of the 4 types
+//
+// A_Z          : Will sort A to Z
+//
+// Z_A          : Will sort Z to A
+//
+// JAN_DEC      : Will sort January to December
+//
+// DEC_JAN      : Will sort December to January
+//
 
 public enum OrganizeType {A_Z, Z_A, JAN_DEC, DEC_JAN}
