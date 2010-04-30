56f604827a38123441b9887105ac78b6152b8df2
Soljia <Soljia@6d042aa6-1d97-11df-9e2b-99333c9bc362>
2010-04-30 14:02:55 +0000

    Commented

--- trunk/Twittah/src/Changes/Search.java
+++ trunk/Twittah/src/Changes/Search.java
@@ -1,55 +1,86 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Project      : IST240 - Twitter Application
+//
+// Class Name   : Search
+//    
+// Authors      : Scott Smiesko, Rick Humes
+// Date         : 2010-30-04
+//
+//
+// DESCRIPTION
+// This class is used to be able to display a search on our subscriptions list. Hence, implements SubscriptionItem.
+//
+// KNOWN LIMITATIONS
+// None.
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 package Changes;
 
-import java.awt.Graphics;
 import java.awt.Image;
-import java.awt.image.BufferedImage;
 import java.net.MalformedURLException;
 import java.net.URL;
-
 import javax.swing.ImageIcon;
-
 import backend.XMLHelper;
-
 import Timelines.SearchTimeline;
 import Timelines.Timeline;
 
 public class Search implements SubscriptionItem{
+    
+    // This class has 3 components used to store information about the search term.
+    //
+    // _query           : The query that was submitted.
+    //
+    // image            : The image that is associated with this, which will ALWAYS be a Magnifying Glass.
+    //
+    // timeline         : The timeline that is associated with this SubscriptionItem
+    //
+    //
+    private String _query;
+    private ImageIcon image;
+    private SearchTimeline timeline;
+    
+    //The class constructor that will take in a query and automatically go to work downloading and parsing the timeline.
+    public Search(String query)
+    {
+        _query = query;
+        try 
+        {
+            image = new ImageIcon(new URL("http://bimmershopper.com/wp-content/uploads/2009/11/Search-icon-256.png"));
+            Image test = image.getImage();
+            test = test.getScaledInstance(50, 50, java.awt.Image.SCALE_SMOOTH);
+            image = new ImageIcon(test);
+        } 
+        catch (MalformedURLException e) 
+        {
+            e.printStackTrace();
+        }
+        //Does the actual downloading and parsing.
+        timeline = SearchTimeline.parseFromDocument(XMLHelper.getTweetsByKeywords(query), query);
+    }
+    
+    // This method will return the items icon.
+    @Override
+    public ImageIcon icon() {
+            return image;
+    }
 
-	private String _query;
-	private ImageIcon image;
-	private SearchTimeline timeline;
-	
-	public Search(String query)
-	{
-	    _query = query;
-		try {
-			image = new ImageIcon(new URL("http://bimmershopper.com/wp-content/uploads/2009/11/Search-icon-256.png"));
-			Image test = image.getImage();
-			test = test.getScaledInstance(50, 50, java.awt.Image.SCALE_SMOOTH);
-			image = new ImageIcon(test);
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		timeline = SearchTimeline.parseFromDocument(XMLHelper.getTweetsByKeywords(query), query);
-	}
-	@Override
-	public ImageIcon icon() {
-		return image;
-	}
+    // This method will return the items identifier, in this case the query.
+    @Override
+    public String text() {
+            return _query;
+    }
 
-	@Override
-	public String text() {
-		return _query;
-	}
-
-	@Override
-	public boolean isSearch() {
-		return true;
-	}
-	@Override
-	public Timeline timeline() {
-		return timeline;
-	}
+    // This method will return whether or not this item is a search, which it is! So, true always.
+    @Override
+    public boolean isSearch() {
+            return true;
+    }
+    
+    // This method will return the timeline associated with this item.
+    @Override
+    public Timeline timeline() {
+            return timeline;
+    }
 
 }
