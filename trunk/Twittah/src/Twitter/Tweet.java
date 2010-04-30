22af8000fabda912f413786eb08fe94c8e653a6e
scott.smiesko <scott.smiesko@6d042aa6-1d97-11df-9e2b-99333c9bc362>
2010-04-30 12:38:48 +0000

    tweet, ButtonManager, and Tweeter commented (Tweeter still needs modified)

--- trunk/Twittah/src/Twitter/Tweet.java
+++ trunk/Twittah/src/Twitter/Tweet.java
@@ -1,77 +1,91 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Project      : IST240 - Twitter Application
+//
+// Class Name   : Tweet
+//    
+// Authors      : Scott Smiesko, Rick Humes
+// Date         : 2010-30-04
+//
+//
+// DESCRIPTION
+// This class holds the information for a specific tweet.  It is constructed by passing the tweeter who posted
+// the tweet, the text content of the tweet, the date the tweet was posted, and the method of posting the tweet.
+//
+// KNOWN LIMITATIONS
+// None.
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 package Twitter;
 
-//Import statements
 import java.util.Date;
 import javax.swing.ImageIcon;
 import Changes.DisplayItem;
 
+public class Tweet implements DisplayItem {
 
-//This is the tweet object!
+    // This class has 4 components used to store information about a tweet:
+    //
+    // tweeter          : The tweeter object who posted the tweet.
+    //
+    // tweetDate        : The date string in UTC for when the tweet was posted.
+    //
+    // tweetText        : The 140 character text content of the tweet.
+    //
+    // tweetMethod      : The method used to post the tweet (like Seesmic)
+    //
+    //
+    private Tweeter tweeter     = null;
+    private Date    tweetDate   = null;
+    private String  tweetText   = null;
+    private String  tweetMethod = null;
 
-public class Tweet implements DisplayItem
-{
-	
-// Class Variables
-    
-    //The tweeter that did the tweeting
-    private Tweeter tweeter = null;
-    
-    //Tweet date
-    private Date tweetDate = null;
-
-    //Tweet text
-    private String tweetText = null;
-    
-    //How the tweet was tweeted
-    private String tweetMethod = null;
-    
-    
-// Class Constructors
-    
-    
-    //This class constructor allows for creation of a tweet, passing in all data.
-    public Tweet(Tweeter newTweeter, String newText, Date newDate, String newMethod)
-    {
-        //The setting of the variables
-    	tweeter = newTweeter;
-    	tweetText = newText;
-    	tweetDate = newDate;
-    	tweetMethod = newMethod;
+    // This class constructor allows for creation of a tweet, passing in all data.
+    //
+    public Tweet(Tweeter newTweeter, String newText, Date newDate, String newMethod) {
+        
+        // Set our variables with the incoming information
+        //
+        tweeter = newTweeter;
+        tweetText = newText;
+        tweetDate = newDate;
+        tweetMethod = newMethod;
     }
     
+    
+    // The following methods override the interface DisplayItem 
+    //
 
-//Class Methods
-    
-    //The following method override DisplayItem
-    
-    //Returns the date at which this tweet was tweeted
+    // Returns the date at which this tweet was tweeted
+    //
     @Override
-    public Date date() 
-    {
+    public Date date() {
         return tweetDate;
     }
-    
-    //Returns the method by which the tweet was made
+
+    // Returns the method by which the tweet was made
+    //
     @Override
-    public String source()
-    {
+    public String source() {
         return tweetMethod;
     }
-    
-    //Returns the text of the tweet
+
+    // Returns the text of the tweet
+    //
     @Override
-    public String text()
-    {
+    public String text() {
         return tweetText;
     }
 
-    //Returns the tweeter's icon
+    // Returns the tweeter's icon
+    //
     @Override
     public ImageIcon icon() {
         return tweeter.icon();
     }
 
-    //Returns the user's screen name
+    // Returns the user's screen name
+    //
     @Override
     public String owner() {
         return tweeter.text();
--- trunk/Twittah/src/Twitter/Tweeter.java
+++ trunk/Twittah/src/Twitter/Tweeter.java
@@ -1,6 +1,25 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Project      : IST240 - Twitter Application
+//
+// Class Name   : Tweeter
+//    
+// Authors      : Scott Smiesko, Rick Humes
+// Date         : 2010-30-04
+//
+//
+// DESCRIPTION
+// This class stores and parses information about a tweeter.  It can either be constructed by using a name, or 
+// by passing all of the required information to it directly (i.e. when the program loads up and displays a users
+// timeline already saved to disk).  Thread-safe and implements programStateListeners/Senders to know when things
+// have been created or destroyed.
+//
+// KNOWN LIMITATIONS
+// None.
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 package Twitter;
 
-//Import Statements
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -14,162 +33,147 @@ import Timelines.Timeline;
 import Timelines.UserTimeline;
 import backend.XMLHelper;
 
+@SuppressWarnings("unchecked")
+public class Tweeter implements SubscriptionItem {
 
-//The tweeter class!
-public class Tweeter implements SubscriptionItem
-{
-	
-//Class Variables
-
-    //Stores the real name of the user
-    private String _userName = null;
-    
-    //Stores the tweeter's profile picture.
-    private ImageIcon _userPicture = null;
-    
-    //Stores the timeline for this user.
+    // This class has 3 components used to manage all the requests
+    // 
+    // _userName                :  A String to store the name of the tweeter.
+    //
+    // _userPicture             :  An ImageIcon to store the profile picture of the tweeter.
+    //
+    // _userTimeline            :  The timeline for that specific user.
+    //
+    // _tweeterXML              :  The XML document that has the tweeters information.
+    //
+    // _listeners               :  For ProgramStateListener.  It holds every object that is listening for the
+    //                             the "tweeter parsing complete" event.
+    //
+    // thread                   :  A thread used to download and parse the tweeter data without interrupting 
+    //                             the program.
+    //
+    //
+    private String       _userName     = null;
+    private ImageIcon    _userPicture  = null;
     private UserTimeline _userTimeline = null;
-    
-    //Document object for storing the XML
-    private Document _tweeterXML = null;
-    
-    //This thread is used to download and parse the tweeter data, without interrupting the program
+    private Document     _tweeterXML   = null;
+    private List         _listeners    = new ArrayList();
     private Thread thread = (new Thread() {
         @SuppressWarnings("deprecation")
         public void run() {
             getXML();
-            if(_tweeterXML != null)
+            if (_tweeterXML != null)
                 parseXML();
             suspend();
-        }
-    });
-    
-    //This is for the ProgramStateListener, it holds every object that is listening for the tweeter parsing complete event.
-    @SuppressWarnings("unchecked")
-    private List _listeners = new ArrayList();
+            }
+        });
 
-    
-//Class Constructors
-    
-    //Constructor method that requires a user name.  
-    public Tweeter(String userName)
-    {
-        //Initializing all the variables
-    	_userName = userName;
-    	
-        //Creates the user's timeline
+    // A constructor method that only requires a username to download and parse a tweeter
+    public Tweeter(String userName) {
+        // Initializing all the variables
+        _userName = userName;
+
+        // Creates the user's timeline
         _userTimeline = new UserTimeline(this);
-    
-    	//Starts the thread which downloads and parses the information
-    	thread.start();
-    }
-    
-    //This constructor allows for the creation of a user without having to download the user's information.
-    public Tweeter(String userName, ImageIcon userPicture, Timeline userTimeline)
-    {
-        //Assigning the inputed variables
-    	_userName = userName;
-    	_userPicture = userPicture;
-    	_userTimeline = (UserTimeline)userTimeline;
-    	
-    	
-    	//Assign a default picture if none is specified
-    	if(_userPicture == null)
-    	{
-    	    try 
-    	    {
-    	        _userPicture = new ImageIcon(new URL("http://s.twimg.com/a/1271891196/images/default_profile_5_normal.png"));
-	    } 
-	    catch (MalformedURLException e) 
-	    {
-	        e.printStackTrace();
-	    }
-    	}
-    	
-    	//Let the listeners know that the Tweeter object is completed.
-    	tweeterParsed();
+
+        // Starts the thread which downloads and parses the information
+        thread.start();
     }
 
-//Class Methods
+    // This constructor allows for the creation of a user without having to download the user's information.
+    public Tweeter(String userName, ImageIcon userPicture, Timeline userTimeline) {
+        // Assigning the inputed variables
+        _userName = userName;
+        _userPicture = userPicture;
+        _userTimeline = (UserTimeline) userTimeline;
 
-    //Get the XML associated to the userName provided
-    private void getXML()
-    {
-        _tweeterXML = XMLHelper.getUserInfoByUserSN(_userName);		
+        // Assign a default picture if none is specified
+        if (_userPicture == null) {
+            try {
+                _userPicture = new ImageIcon(
+                        new URL("http://s.twimg.com/a/1271891196/images/default_profile_5_normal.png"));
+            }
+            catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Let the listeners know that the Tweeter object is completed.
+        tweeterParsed();
     }
-    
-    //Populates the rest of a tweeter object with data from the XML feed
-    private void parseXML()
-    {
-       	Element user = (Element)(_tweeterXML.getFirstChild());
-       	Element picture = (Element)(user.getElementsByTagName("profile_image_url").item(0));
-       	
-       	//Try to download and set the user's picture
-        try 
-        {
+
+    // Class Methods
+
+    // Get the XML associated to the userName provided
+    private void getXML() {
+        _tweeterXML = XMLHelper.getUserInfoByUserSN(_userName);
+    }
+
+    // Populates the rest of a tweeter object with data from the XML feed
+    private void parseXML() {
+        Element user = (Element) (_tweeterXML.getFirstChild());
+        Element picture = (Element) (user
+                .getElementsByTagName("profile_image_url").item(0));
+
+        // Try to download and set the user's picture
+        try {
             _userPicture = new ImageIcon(new URL(picture.getTextContent()));
-        } 
-        catch (MalformedURLException e) 
-        {
+        }
+        catch (MalformedURLException e) {
             e.printStackTrace();
         }
-        
-        //Let the listeners know that the Tweeter object is completed.
+
+        // Let the listeners know that the Tweeter object is completed.
         tweeterParsed();
-       	
+
     }
-    
-    //Returns the user's picture
+
+    // Returns the user's picture
     @Override
-    public ImageIcon icon() 
-    {
+    public ImageIcon icon() {
         return _userPicture;
     }
 
-    //Returns the user's name
+    // Returns the user's name
     @Override
     public String text() {
         return _userName;
     }
 
-    //Returns that this DisplayItem is not a search
+    // Returns that this DisplayItem is not a search
     @Override
     public boolean isSearch() {
         return false;
     }
 
-    //Return the user's timeline
+    // Return the user's timeline
     @Override
     public Timeline timeline() {
         return _userTimeline;
     }
 
-    
-    //This is for threading
-    
-    //Adds a listener
+    // This is for threading
+
+    // Adds a listener
     @SuppressWarnings("unchecked")
-    public synchronized void addProgramStateListener( ProgramStateListener l ) 
-    {
+    public synchronized void addProgramStateListener(ProgramStateListener l) {
         _listeners.add(l);
     }
-    
-    //Removes a listener
-    public synchronized void removeProgramStateListener( ProgramStateListener l ) 
-    {
+
+    // Removes a listener
+    public synchronized void removeProgramStateListener(ProgramStateListener l) {
         _listeners.remove(l);
     }
-    
-    //Informs listeners that the tweeter object is completed.
+
+    // Informs listeners that the tweeter object is completed.
     @SuppressWarnings("unchecked")
-    private synchronized void tweeterParsed()
-    {
+    private synchronized void tweeterParsed() {
         ProgramStateEvent state = new ProgramStateEvent(this, ProgramState.SUBSCRIPTION_ADDED);
         ProgramStateListener[] listeners = new ProgramStateListener[_listeners.size()];
         _listeners.toArray(listeners);
+
+        for (ProgramStateListener listener : listeners)
+            listener.stateReceived(state);
         
-        for(ProgramStateListener listener : listeners)
-        	listener.stateReceived(state);
     }
-    
 }
