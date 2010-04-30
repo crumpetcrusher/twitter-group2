a370e520570f8f4759d398d73949a1530111a0e8
Soljia <Soljia@6d042aa6-1d97-11df-9e2b-99333c9bc362>
2010-04-30 14:21:43 +0000

    Commented

--- Twittah/src/Timelines/CompositeTimeline.java
+++ Twittah/src/Timelines/CompositeTimeline.java
@@ -1,103 +1,118 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Project      : IST240 - Twitter Application
+//
+// Class Name   : CompositeTimeline
+//    
+// Authors      : Scott Smiesko, Rick Humes
+// Date         : 2010-30-04
+//
+//
+// DESCRIPTION
+// This is a extention of Timeline which will store multiple timelines. The state listener is for the listening of
+// whether or not a timeline is done downloading and parsing.
+//
+// KNOWN LIMITATIONS
+// None.
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 package Timelines;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-
-
 import Changes.DisplayItem;
-import Changes.DisplayItemOrganizer;
-import Changes.OrganizeType;
 import ThreadingHelpers.ProgramState;
 import ThreadingHelpers.ProgramStateEvent;
 import ThreadingHelpers.ProgramStateListener;
-import Twitter.Tweet;
 
 public class CompositeTimeline extends Timeline implements ProgramStateListener{
 	
-	private List<Timeline> timelines = new ArrayList<Timeline>();
-	
-	public void addTimeline(Timeline timeline)
-	{
-		System.out.println("Timeline Added");
-		if(!timelines.contains(timeline))
-		{
-		    timelines.add(timeline);
-    		    timeline.addProgramStateListener(this);
-    		    fill();
-    		    timelineAdded();
-		}
-	}
-	
-	private synchronized void timelineAdded()
-	{
-		ProgramStateEvent state = new ProgramStateEvent(this, ProgramState.TIMELINE_ADDED);
-		ProgramStateListener[] listeners = new ProgramStateListener[_listeners.size()];
-		_listeners.toArray(listeners);
-            for(ProgramStateListener listener : listeners)
-            {
-            	System.out.println("Sending state" + state.state() + " to" + listener);
-            	listener.stateReceived(state);
-            }
-	}
-	
-	public void clearAll()
-	{
-		clearItems();
-		timelines.clear();
-	}
-	
-	public void fill()
-	{
-		clearItems();
-		for(Timeline timeline : timelines)
-			for(DisplayItem displayItem : timeline.displayItems())
-				addDisplayItem(displayItem);
-		organize();
-	}
+    // This class has 1 component used to store timelines.
+    //
+    // timelines        : Stores the timelines.
+    //
+    private List<Timeline> timelines = new ArrayList<Timeline>();
+    
+    // This method allows a timeline to be passed in and added to our list.
+    public void addTimeline(Timeline timeline)
+    {
+        // Won't add the timeline if it exists
+        if(!timelines.contains(timeline))
+        {
+            timelines.add(timeline);
+            timeline.addProgramStateListener(this);
+            fill();
+            timelineAdded();
+        }
+    }
 
-	public void removeTimeline(Timeline timeline) {
+    // Clears all display items and timelines stored.
+    public void clearAll()
+    {
+        clearItems();
+        timelines.clear();
+    }
+    
+    // Clears the display items and fills in according to what's in the timelines
+    public void fill()
+    {
+        clearItems();
+        for(Timeline timeline : timelines)
+                for(DisplayItem displayItem : timeline.displayItems())
+                        addDisplayItem(displayItem);
+        organize();
+    }
 
-		for(Timeline temp : timelines) {
-			if (temp.equals(timeline)){
-				timelines.remove(timeline);
-			}
-		}
-		fill();
-		
-	}
-	
-	protected void reload() {}
+    // Removes a timeline from our list
+    public void removeTimeline(Timeline timeline) 
+    {
+        for(Timeline temp : timelines) 
+            if (temp.equals(timeline))
+                timelines.remove(timeline);
+        fill();   
+    }
 
-	@Override
-	public void downloadAndParse() {
-		for(Timeline timeline :  timelines)
-		{
-			timeline.clearItems();
-			timeline.downloadAndParse();
-		}
-		//fill();
-		//timelineRefreshed();
-	}
-
-	@Override
-	public void saveTimeline() {
-		for(Timeline timeline : timelines)
-			timeline.saveTimeline();
-	}
-
+    // An override, meant to download and parse the timeline
+    // As this timeline has multiple, it'll refresh them.
     @Override
-    public void stateReceived(ProgramStateEvent event) {
-        System.out.println("State Received: " + event.state() );
+    public void downloadAndParse() 
+    {
+        for(Timeline timeline :  timelines)
+        {
+            timeline.clearItems();
+            timeline.downloadAndParse();
+        }
+    }
+
+    // Saves the timelines stored, to their respective files.
+    @Override
+    public void saveTimeline() {
+            for(Timeline timeline : timelines)
+                    timeline.saveTimeline();
+    }
+    
+    protected void reload() {}
+
+    // This method is called be the listenee, aka timeline, when it is done downloading and parsing.
+    // Which when this is called, it will fill the displayitems in and refresh the timeline.
+    @Override
+    public void stateReceived(ProgramStateEvent event) 
+    {
         if(event.state() == ProgramState.TIMELINE_ADDED || event.state() == ProgramState.TIMELINE_REFRESHED)
         {
-            System.out.println("Timeline Refreshed!");
             fill();
             timelineRefreshed();
         }
+    }
+    
+    // Called when a timeline is added, which will inform all listeners of this action has happened.
+    private synchronized void timelineAdded()
+    {
+        ProgramStateEvent state = new ProgramStateEvent(this, ProgramState.TIMELINE_ADDED);
+        ProgramStateListener[] listeners = new ProgramStateListener[_listeners.size()];
+        _listeners.toArray(listeners);
+        for(ProgramStateListener listener : listeners)
+            listener.stateReceived(state);
     }
 
 	
