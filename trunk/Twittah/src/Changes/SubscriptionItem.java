d2dcaa1f7a300372379194b6c500abbbf20d4b43
Soljia <Soljia@6d042aa6-1d97-11df-9e2b-99333c9bc362>
2010-04-30 14:06:23 +0000

    Commented

--- trunk/Twittah/src/Changes/SubscriptionItem.java
+++ trunk/Twittah/src/Changes/SubscriptionItem.java
@@ -1,15 +1,42 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Project      : IST240 - Twitter Application
+//
+// Interface Name   : SubscriptionItem
+//    
+// Authors      : Scott Smiesko, Rick Humes
+// Date         : 2010-30-04
+//
+//
+// DESCRIPTION
+// This interface is the template for being able to be displayed on our subscription list.
+//
+// KNOWN LIMITATIONS
+// None.
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 package Changes;
 
 import Timelines.Timeline;
 
 public interface SubscriptionItem {
 
-	public String text();
-	
-	public javax.swing.ImageIcon icon();
-	
-	public boolean isSearch();
-	
-	public Timeline timeline();
+    // This class has 3 components used to store information about the search term.
+    //
+    // text()           : The identifier, usually the search term or a tweeters name.
+    //
+    // icon()           : The icon of the tweeter, or a magnifying glass for a search. (as of now)
+    //
+    // isSearch()       : Whether or not this item is a search. Search - True, Tweeter - False
+    //
+    // timeline()       : The timeline associated with this item.
+    //
+    public String text();
+    
+    public javax.swing.ImageIcon icon();
+    
+    public boolean isSearch();
+    
+    public Timeline timeline();
 	
 }
