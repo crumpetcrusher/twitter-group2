c910b6bcc63e3a7e1f4163ad6003b408c89899d5
Soljia <Soljia@6d042aa6-1d97-11df-9e2b-99333c9bc362>
2010-05-01 03:13:43 +0000

    Commenting and Diagrams

--- Twittah/src/Timelines/Timeline.java
+++ Twittah/src/Timelines/Timeline.java
@@ -1,105 +1,153 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Project               : IST240 - Twitter Application
+//
+// Abstract Class Name   : Timeline
+//    
+// Authors               : Scott Smiesko, Rick Humes
+// Date                  : 2010-30-04
+//
+//
+// DESCRIPTION
+// This is the backbone of all timelines used in the program which holds all the common information that a
+// timeline would require.  It stores a list of DisplayItems (which is how usertimeline and searchTimeline are
+// able to store their tweets as they inherit the abilities from this class) and has all the necessary means 
+// of refreshing/deleting/adding/sorting displayitems for use in other timelines.
+//
+// KNOWN LIMITATIONS
+// None.
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 package Timelines;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-
-import backend.AutomaticTimelineRefresher;
-
-import ThreadingHelpers.ProgramState;
-import ThreadingHelpers.ProgramStateEvent;
-import ThreadingHelpers.ProgramStateListener;
-
 
 import Changes.DisplayItem;
 import Changes.DisplayItemOrganizer;
 import Changes.OrganizeType;
+import ThreadingHelpers.ProgramState;
+import ThreadingHelpers.ProgramStateEvent;
+import ThreadingHelpers.ProgramStateListener;
 
 public abstract class Timeline {
-	
-	protected List<DisplayItem> displayItems = new ArrayList<DisplayItem>();
-	
-	protected List _listeners = new ArrayList();
-	
-	private OrganizeType currentOrganize  = OrganizeType.JAN_DEC;
-	
-	protected Document timelineXML = null;
-	
-	public DisplayItem[] displayItems()
-	{
-		return displayItems.toArray(new DisplayItem[displayItems.size()]);
-	}
-	
-	protected void addDisplayItem(DisplayItem newDisplayItem)
-	{
-		displayItems.add(newDisplayItem);
-		timelineModified();
-	}
-	
-	public void clearItems()
-	{
-		displayItems.clear();
-	}
-	
-	public String toString()
-	{
-		String value = "[Timeline]" + "\r\n";
-		for(DisplayItem displayItem : displayItems)
-			value += "\t" + displayItem.toString() + "\r\n";
-		return value;
-	}
-	
-	public OrganizeType getOrganizeType()
-	{
-		return currentOrganize;
-	}
-	
-	public void organize()
-	{
-		Collections.sort(displayItems, new DisplayItemOrganizer(currentOrganize));
-		timelineModified();
-	}
 
-	public void setOrganizeType(OrganizeType type) {
-		currentOrganize = type;
-	}
-        
-        // Called when a timeline is added, which will inform all listeners of this action has happened.
-        //
-        @SuppressWarnings("unchecked")
-        protected synchronized void timelineModified()
-        {
-            ProgramStateEvent state = new ProgramStateEvent(this, ProgramState.TIMELINE_MODIFIED);
-            ProgramStateListener[] listeners = new ProgramStateListener[_listeners.size()];
-            _listeners.toArray(listeners);
-            for(ProgramStateListener listener : listeners)
-                listener.stateReceived(state);
-        }
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Abstract Class Attributes
+    //
+    
+    // This class has 4 attributes used by a timeline
+    // 
+    // displayItems             :  Stores the tweet messages that would go into a timeline
+    //
+    // _listeners               :  Stores all the current listeners needed for threading to work alongside all
+    //                             other classes
+    //
+    // currentOrganize          :  Stores the way the timeline should organize itself.  Can be 
+    //                             ascending/descending by alphabetical or by date.
+    //
+    // timelineXML              :  A storage place for the XML feed downloaded from twitter.
+    //
+    //
+    protected List<DisplayItem> displayItems    = new ArrayList<DisplayItem>();
+    protected List              _listeners      = new ArrayList();
+    private   OrganizeType      currentOrganize = OrganizeType.JAN_DEC;
+    protected Document          timelineXML     = null;
 
+    
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Abstract Class Methods
+    //
+    
+    // This method returns all of the display items stored in a timeline
+    //
+    public DisplayItem[] displayItems() {
+        return displayItems.toArray(new DisplayItem[displayItems.size()]);
+    }
 
-	public abstract void downloadAndParse();
-	
-	public abstract void saveTimeline();
-	
-	   @SuppressWarnings("unchecked")
-	public synchronized void addProgramStateListener( ProgramStateListener l ) {
-	        if(_listeners.add( l ))
-	 		   System.out.println("Listener Added");
-	        else
-	 		   System.out.println("Listener Not Added");
-	        	
-	    }
-	    
-	    public synchronized void removeProgramStateListener( ProgramStateListener l ) {
-	        _listeners.remove( l );
-	    }
-	
+    // This method adds a displayItem to the list of displayItems
+    //
+    protected void addDisplayItem(DisplayItem newDisplayItem) {
+        displayItems.add(newDisplayItem);
+        timelineModified();
+    }
+
+    // This will clear all the items in displayItems
+    //
+    public void clearItems() {
+        displayItems.clear();
+    }
+
+    // Displays the list of displayItems as a string for console output
+    //
+    public String toString() {
+        String value = "[Timeline]" + "\r\n";
+        for (DisplayItem displayItem : displayItems)
+            value += "\t" + displayItem.toString() + "\r\n";
+        return value;
+    }
+
+    // Returns the current way we are organizing the timeline.
+    //
+    public OrganizeType getOrganizeType() {
+        return currentOrganize;
+    }
+
+    // Called to organize the timeline if new displayItems are added.
+    //
+    public void organize() {
+        Collections.sort(displayItems, new DisplayItemOrganizer(currentOrganize));
+        timelineModified();
+    }
+
+    // Sets the way we are to be organizing the timeline
+    //
+    public void setOrganizeType(OrganizeType type) {
+        currentOrganize = type;
+    }
+
+    // Abstract method to downloadAndParse the new information from an XML feed
+    //
+    public abstract void downloadAndParse();
+
+    // Abstract method to save the timeline to disk
+    //
+    public abstract void saveTimeline();
+    
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Abstract Class Threading
+    //
+
+    // Called when a timeline is added, which will inform all listeners of this action has happened.
+    //
+    @SuppressWarnings("unchecked")
+    protected synchronized void timelineModified() {
+        ProgramStateEvent state = new ProgramStateEvent(this, ProgramState.TIMELINE_MODIFIED);
+        ProgramStateListener[] listeners = new ProgramStateListener[_listeners.size()];
+        _listeners.toArray(listeners);
+        for (ProgramStateListener listener : listeners)
+            listener.stateReceived(state);
+    }
+    
+    // Used to add a programStateListener so that listener will be informed when timelineModified()
+    // above has happened.
+    //
+    @SuppressWarnings("unchecked")
+    public synchronized void addProgramStateListener(ProgramStateListener l) {
+        if (_listeners.add(l))
+            System.out.println("Listener Added");
+        else
+            System.out.println("Listener Not Added");
+
+    }
+
+    // Removes a programStateListener when it's no longer required to be listening to this class
+    //
+    public synchronized void removeProgramStateListener(ProgramStateListener l) {
+        _listeners.remove(l);
+    }
+
 }
-	
-
-
-	
--- Twittah/src/backend/TimelinesManager.java
+++ Twittah/src/backend/TimelinesManager.java
@@ -1,16 +1,32 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Project              Ê: IST240 - Twitter Application
+//
+// Class Name            : TimelinesManager
+//    
+// Authors               : Scott Smiesko, Rick Humes
+// Date                  : 2010-30-04
+//
+//
+// DESCRIPTION
+// This class handles the management of timelines that are being displayed to the user.  It holds an object
+// of type CompositeTimeline which stores multiple timelines which can be sorted by the users choice.  It
+// is the object that gets called to manipulate the TimelinesViewer based on what subscriptionsManager says 
+// needs to be being displayed at the current time.
+//
+// KNOWN LIMITATIONS
+// None.
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 package backend;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-
 
 import Changes.OrganizeType;
-import Changes.Search;
 import Changes.SubscriptionItem;
 import GUI.RootGUI;
 import GUI.TimelinesViewer;
@@ -20,128 +36,183 @@ import ThreadingHelpers.ProgramStateListener;
 import Timelines.CompositeTimeline;
 import Timelines.SearchTimeline;
 import Timelines.Timeline;
-import Timelines.UserTimeline;
 import Twitter.Tweeter;
 
 public class TimelinesManager implements ProgramStateListener {
-	
-	private SubscriptionsManager subscriptionsMgr;
-	private TimelinesViewer		 timelinesVwr;
-	private CompositeTimeline compositeTimeline = new CompositeTimeline();
-	private AutomaticTimelineRefresher refresher = new AutomaticTimelineRefresher(compositeTimeline, 30);
 
-	public TimelinesManager(SubscriptionsManager newSubscriptionsMgr, RootGUI gui) {
-		compositeTimeline.addProgramStateListener(this);
-		timelinesVwr = new TimelinesViewer(this);
-		subscriptionsMgr = newSubscriptionsMgr;
-		loadPreviousTimelines();
-		gui.add(timelinesVwr);
-	}
-	
-	protected void initialize() {
-		for (SubscriptionItem subscriptItem : subscriptionsMgr.getSubscriptions())     
-	            addToTimeline(subscriptItem);
-	}
-	
-	public void toggleAutomaticRefresh()
-	{
-            if(refresher.isPaused())
-                refresher.Resume();
-            else
-                refresher.Suspend();
-	}
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Class Attributes
+    //
+    
+    // This class holds 4 attributes which aid in the viewing of a composite timeline in the GUI
+    // 
+    // subscriptionsMgr                 :  The reference for our subscriptions manager; this class uses it to 
+    //                                     pull subscriptions from our database of subscriptions and update the
+    //                                     composite timeline with whoever we need to display at a given time.
+    //
+    // timelinesVwr                     :  The reference for our timelinesViewer GUI object.  This is used for 
+    //                                     TimelinesManager to be able to display the current feeds available in
+    //                                     its compositeTimeline attribute.
+    //
+    // compositeTimeline                :  A subclass of Timeline to store multiple timelines for viewing in
+    //                                     the GUI with timelinesVwr.
+    //
+    // refresher                        :  An extension of a Thread. used for automatically refreshing the 
+    //                                     timelines currently in the compositeTimeline
+    //
+    //
+    private SubscriptionsManager       subscriptionsMgr;
+    private TimelinesViewer            timelinesVwr;
+    private CompositeTimeline          compositeTimeline = new CompositeTimeline();
+    private AutomaticTimelineRefresher refresher         = new AutomaticTimelineRefresher(compositeTimeline, 30);
 
-	public Timeline getCompositeTimeline() {
-		return compositeTimeline;
-	}
-	
-	public void organize(OrganizeType type)
-	{
-		compositeTimeline.setOrganizeType(type);
-		compositeTimeline.organize();
-	}
-	
-	public void clearTimeline() {
-		compositeTimeline.clearAll();
-	}
-	
-	public OrganizeType organizeType()
-	{
-		return compositeTimeline.getOrganizeType();
-	}
-	
-	public void addToTimeline(SubscriptionItem subscriptItemToAdd) {
-	    Timeline subscriptTimeline = subscriptItemToAdd.timeline();
-            compositeTimeline.addTimeline(subscriptTimeline);
-	}
-	
-	public void refreshTimeline() 
-	{
-            compositeTimeline.downloadAndParse();
-	}
-	
-	public void addTimeline(Timeline timeline)
-	{
-	    compositeTimeline.addTimeline(timeline);
-	}
-	
-	public void saveTimelines()
-	{
-	    compositeTimeline.saveTimeline();
-	}
-	
-	public void loadPreviousTimelines()
-	{
-		System.out.println("Loading Previous Timelines");
-		File dir = new File("src");
-		String[] files = dir.list();
-		Document doc = null;
-	        Matcher matcher;
-	        Timeline timeline = null;
-	        SubscriptionItem sub = null;
-		
-		for(String file : files)
-			if(file.contains("timeline") && file.contains(".xml"))
-			{
-		                matcher = Pattern.compile("(?<=\\_).+?(?=\\.xml)").matcher(file);
-		                if(matcher.find())
-		                {
-        		                System.out.println("Loading " + matcher.group(0));
-        				doc = XMLHelper.getDocumentByLocation("src/" + file);
-        				if(file.contains("user"))
-        				    //timeline = UserTimeline.parseFromDocument(matcher.group(0), doc);
-        				    sub = Tweeter.getTweeterFromDoc(doc);
-        				else
-        				    timeline = SearchTimeline.parseFromDocument(matcher.group(0), doc);
-        				
-        				if(timeline != null)
-        				    addTimeline(timeline);
-        				if(sub  != null)
-        				    addToTimeline(sub);
-		                }
-			}
-	}
-	
-	public void deletePreviousTimelines()
-	{
-		File dir = new File("src");
-		String[] files = dir.list();
-		Document doc = null;
-		
-		for(String file : files)
-			if(file.contains("timeline") && file.contains(".xml"))
-			{
-				File temp = new File("src/" + file);
-				if(temp.delete())
-					System.out.println("File Deleted: " + file);
-				else
-					System.out.println(file + " not delteed!");
-			}
-	}
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Class Constructors
+    //
+    
+    // This constructor is passed in the subscriptionsManager and RootGUI and creates the necessary environment
+    // for compositeTimeline to work and the timelinesViewer to display the timelines
+    //
+    public TimelinesManager(SubscriptionsManager newSubscriptionsMgr, RootGUI gui) {
+        compositeTimeline.addProgramStateListener(this);
+        timelinesVwr = new TimelinesViewer(this);
+        subscriptionsMgr = newSubscriptionsMgr;
+        loadPreviousTimelines();
+        gui.add(timelinesVwr);
+    }
 
-	@Override
-	public void stateReceived(ProgramStateEvent event) {
-	    if(event.state() == ProgramState.TIMELINE_MODIFIED || event.state() == ProgramState.TIMELINE_REFRESHED)
-	        timelinesVwr.refresh();
-	}
+    
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Class Methods
+    //
+    
+    // This is used to initialize..
+    //
+    protected void initialize() {
+        for (SubscriptionItem subscriptItem : subscriptionsMgr.getSubscriptions())
+            addToTimeline(subscriptItem);
+    }
+
+    // This is to toggle the automatic refresh of the composite timeline
+    //
+    public void toggleAutomaticRefresh() {
+        if (refresher.isPaused())
+            refresher.Resume();
+        else
+            refresher.Suspend();
+    }
+
+    // Returns the compositeTimeline of whatever users we are viewing
+    // 
+    public Timeline getCompositeTimeline() {
+        return compositeTimeline;
+    }
+
+    // Sets the way the compositeTimeline should be organized by passing in
+    // an enum OrganizeType
+    //
+    public void organize(OrganizeType type) {
+        compositeTimeline.setOrganizeType(type);
+        compositeTimeline.organize();
+    }
+
+    // Clears all the timelines and displayItems stored from the compositeTimeline
+    //
+    public void clearTimeline() {
+        compositeTimeline.clearAll();
+    }
+
+    // Returns the current organize type in use
+    // 
+    public OrganizeType organizeType() {
+        return compositeTimeline.getOrganizeType();
+    }
+
+    // Adds a subscriptionItem to the Timeline for viewing (search/tweeter)
+    //
+    public void addToTimeline(SubscriptionItem subscriptItemToAdd) {
+        Timeline subscriptTimeline = subscriptItemToAdd.timeline();
+        compositeTimeline.addTimeline(subscriptTimeline);
+    }
+
+    // Refreshes the entire compositeTimeline.  Every user/serach timeline is redownloaded
+    // and the compositeTimeline is refreshed
+    //
+    public void refreshTimeline() {
+        compositeTimeline.downloadAndParse();
+    }
+
+    // Adds an entire timeline to the CompositeTimeline
+    //
+    public void addTimeline(Timeline timeline) {
+        compositeTimeline.addTimeline(timeline);
+    }
+
+    // Saves the timeline to disk
+    //
+    public void saveTimelines() {
+        compositeTimeline.saveTimeline();
+    }
+
+    // Loads the previous timelines the user was viewing when the program last closed
+    // 
+    public void loadPreviousTimelines() {
+        System.out.println("Loading Previous Timelines");
+        File dir = new File("src");
+        String[] files = dir.list();
+        Document doc = null;
+        Matcher matcher;
+        Timeline timeline = null;
+        SubscriptionItem sub = null;
+
+        for (String file : files)
+            if (file.contains("timeline") && file.contains(".xml")) {
+                matcher = Pattern.compile("(?<=\\_).+?(?=\\.xml)").matcher(file);
+                if (matcher.find()) {
+                    System.out.println("Loading " + matcher.group(0));
+                    doc = XMLHelper.getDocumentByLocation("src/" + file);
+                    if (file.contains("user"))
+                        // timeline = UserTimeline.parseFromDocument(matcher.group(0), doc);
+                        sub = Tweeter.getTweeterFromDoc(doc);
+                    else
+                        timeline = SearchTimeline.parseFromDocument(matcher.group(0), doc);
+
+                    if (timeline != null)
+                        addTimeline(timeline);
+                    if (sub != null)
+                        addToTimeline(sub);
+                }
+            }
+    }
+
+    // Deletes the timelines, that were saved to file on system close, after they are initialized
+    // into the program
+    //
+    public void deletePreviousTimelines() {
+        File dir = new File("src");
+        String[] files = dir.list();
+        Document doc = null;
+
+        for (String file : files)
+            if (file.contains("timeline") && file.contains(".xml")) {
+                File temp = new File("src/" + file);
+                if (temp.delete())
+                    System.out.println("File Deleted: " + file);
+                else
+                    System.out.println(file + " not delteed!");
+            }
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Class Threading
+    //
+    
+    // Used to be a docking station for any other class who needs to notify this class that an event has finished
+    // so the timelinesViewer can be refreshed upon updating.
+    // 
+    @Override
+    public void stateReceived(ProgramStateEvent event) {
+        if (event.state() == ProgramState.TIMELINE_MODIFIED || event.state() == ProgramState.TIMELINE_REFRESHED)
+            timelinesVwr.refresh();
+    }
 }
