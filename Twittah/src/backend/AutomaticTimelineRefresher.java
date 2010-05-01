e41529e44d03bd7361a4902b0171526d98c10320
Soljia <Soljia@6d042aa6-1d97-11df-9e2b-99333c9bc362>
2010-05-01 03:21:44 +0000

    Last bit of commenting

--- Twittah/src/backend/AutomaticTimelineRefresher.java
+++ Twittah/src/backend/AutomaticTimelineRefresher.java
@@ -9,7 +9,7 @@
 //
 //
 // DESCRIPTION
-// HEY PUT A GODDAMN DESCRIPTION IN HERE!!!!!!!!!!!!!!!!!!!!!!
+// 
 //
 // KNOWN LIMITATIONS
 // None.
@@ -19,18 +19,38 @@ package backend;
 
 import Timelines.Timeline;
 
-
 public class AutomaticTimelineRefresher extends Thread {
-    // Must be volatile:
-    private volatile boolean paused = true;
-    private int _refreshRate = 30000;
-    private ButtonManager _buttonMgr;
-    private Timeline _timeline = null;
-    private int counter;
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //  Class Attributes
+    //
     
-    public AutomaticTimelineRefresher(Timeline timeline, int timeInSecToRefresh)
-    {
-        if(timeline == null || timeInSecToRefresh < 1)
+    // There are 4 attributes used in this class to manage automatic refreshing of a timeline:
+    //
+    // paused           :  (must be) Volatile boolean accessable from anywhere in Main memory used to let
+    //                     any other threads know it's paused or not.
+    //
+    // _refreshRate     :  The time to count down from (in milliseconds).
+    //
+    // _timeline        :  Stores the timeline in which to refresh
+    //
+    // counter          :  This will count down from the _refreshRate, as per run()
+    //
+    
+    private volatile boolean paused       = true;
+    private int              _refreshRate = 30000;
+    private Timeline         _timeline    = null;
+    private int              counter;
+
+    
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Class Constructors
+    //
+    
+    // Constructor that will take in a timeline and how often to refresh in seconds
+    //
+    public AutomaticTimelineRefresher(Timeline timeline, int timeInSecToRefresh) {
+        if (timeline == null || timeInSecToRefresh < 1)
             stop();
         _timeline = timeline;
         _refreshRate = timeInSecToRefresh * 1000;
@@ -38,44 +58,51 @@ public class AutomaticTimelineRefresher extends Thread {
         Suspend();
     }
 
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Class Methods
+    //
+    
+    // Override method, this code will run when start() is called
+    //
     public void run() {
-      while (isAlive())
-      {
-          try 
-          {
-              counter = _refreshRate / 1000;
-              while(counter > 0)
-              {
-                  if(counter % 5 == 0)
-                      System.out.println("Refreshing in " + counter);
-                  sleep(1000);
-                  counter--;
-              }
-              _timeline.downloadAndParse();
-          }
-          catch (InterruptedException e) 
-          {
-            e.printStackTrace();
-          }
-          
-      }
+        while (isAlive()) {
+            try {
+                counter = _refreshRate / 1000;
+                // Will count down to 0 from time requested
+                while (counter > 0) {
+                    if (counter % 5 == 0)
+                        System.out.println("Refreshing in " + counter);
+                    sleep(1000);
+                    counter--;
+                }
+                // Downloads and parses the timeline
+                _timeline.downloadAndParse();
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 
-    public void Suspend() 
-    {
+    // Suspends the running of this thread
+    //
+    public void Suspend() {
         paused = true;
         counter = _refreshRate / 1000;
         super.suspend();
     }
-    
-    public void Resume()
-    {
+
+    // Resumes the running of this thread
+    //
+    public void Resume() {
         paused = false;
         super.resume();
     }
-    
-    public boolean isPaused()
-    {  
-      return paused;
+
+    // Lets outsiders know if it is paused
+    //
+    public boolean isPaused() {
+        return paused;
     }
 }
